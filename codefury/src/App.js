ece274700aba875b3afb3dc010bba99e9cf674e2
Varun K J <40955517+ShhhBaby@users.noreply.github.com>
2020-11-08 07:31:14 +0530

    final

--- codefury/src/App.js
+++ codefury/src/App.js
@@ -3,20 +3,22 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import SkillToWorker from "./components/SkillToWorker";
 import { VillageToWorker } from "./components/VillageToWorker";
-import { PieChart } from 'react-minimal-pie-chart';
+import { PieChart } from "react-minimal-pie-chart";
 function App() {
   const [workers, setWorkers] = useState([]);
-  const [showFilter, setShowFilter] =useState(false);
+  const [showFilter, setShowFilter] = useState(false);
   const [search, setSearch] = useState("");
   const [figures, setFigures] = useState({
     constructionNumber: 0,
     securityNumber: 0,
     factoryNumber: 0,
   });
-  const countFac = workers.filter(obj => obj.skills === "factory worker").length
-  const countSec = workers.filter(obj => obj.skills === "security").length
-  const countConst = workers.filter(obj => obj.skills === "construction").length
- 
+  const countFac = workers.filter((obj) => obj.skills === "factory worker")
+    .length;
+  const countSec = workers.filter((obj) => obj.skills === "security").length;
+  const countConst = workers.filter((obj) => obj.skills === "construction")
+    .length;
+
   useEffect(() => {
     fetch("http://covidxrayapi.herokuapp.com/labourData")
       .then((res) => res.json())
@@ -25,65 +27,133 @@ function App() {
         console.log(data.workers);
       });
   }, []);
-  console.log(workers)
+  console.log(workers);
   return (
     <>
       <div>
         <div className="navbar">
-          <h2 style={{
-            fontSize: (window.innerWidth<500?30:42)
-          }}>Dashboard</h2>
-          <div style={{
-            display:"flex",
-            flexWrap:"wrap",
-            padding:10
-          }}>
-          <input
-            type="text"
-            placeholder="Search for skills.."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
+          <h2
             style={{
-              border: "1px solid lightblue",
-              width: (window.innerWidth>500?"50%":"30%"),
-              padding: "10px",
-              borderRadius: "5px",
+              fontSize: window.innerWidth < 500 ? 30 : 42,
             }}
-      />
-          <button onClick={()=>{
-            setShowFilter(!showFilter)
-          }}>{showFilter?"Close" :"Search"}</button> 
+          >
+            Dashboard
+          </h2>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              padding: 10,
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Search for skills.."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              style={{
+                border: "1px solid lightblue",
+                width: window.innerWidth > 500 ? "50%" : "30%",
+                padding: "10px",
+                borderRadius: "5px",
+              }}
+            />
+            <button
+              onClick={() => {
+                setShowFilter(!showFilter);
+              }}
+            >
+              {showFilter ? (
+                "Close"
+              ) : (
+                <i
+                  className="fa fa-search"
+                  style={{ fontSize: "20px", padding: "7px" }}
+                ></i>
+              )}
+            </button>
           </div>
         </div>
-      
-    
+        <h1 style={{ width: "100%", textAlign: "center", fontWeight: "100" }}>
+          Occupation <i className="fa fa-pie-chart"></i>
+        </h1>
+
         <PieChart
-         animate={true}
+          animate={true}
           data={[
-            { title: 'One', value: countSec, color: '#add8e6' },
-            { title: 'Two', value: countFac, color: '#FFB6C1' },
-            { title: 'Three', value: countConst, color: '	#32CD32' }
+            { title: "One", value: countSec, color: "#add8e6" },
+            { title: "Two", value: countFac, color: "#FFB6C1" },
+            { title: "Three", value: countConst, color: "	#32CD32" },
           ]}
-          viewBoxSize = {[300,100]}
-          radius={35}
-      />
-      <div style={{position:"relative",padding:150,width:"50%", display:(window.length>500?"flex":"none"), justifyContent:"center",alignItems:"center", flexWrap:"wrap"}}>
-       <div> <h3 style={{
-         width:"100%"
-       }}>Green : Construction Worker</h3></div>
-      <div style={{
-         width:"100%", textAlign:"center"
-       }}>  <h3>Pink : Factory worker</h3></div>
-       <div style={{
-         width:"100%", textAlign:"center"
-       }}> <h3>Blue : Security Personell</h3></div><br/>
+          viewBoxSize={[240, 100]}
+          radius={40}
+          // label={({ dataEntry }) => dataEntry.value}
+          // labelStyle={{ fontSize: "5px" }}
+        />
+        <div
+          style={{
+            display: window.innerWidth > 500 ? "block" : "none",
+            position: "absolute",
+
+            top: "60%",
+            left: "70%",
+            transform: "translate(-50%,-50%)",
+          }}
+        >
+          <div>
+            {" "}
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                border: "1px solid grey",
+                backgroundColor: "lightgreen",
+              }}
+            ></div>
+            <h2
+              style={{
+                width: "100%",
+              }}
+            >
+              {" "}
+              Construction Worker
+            </h2>
+          </div>
+          <div
+            style={{
+              width: "100%",
+            }}
+          >
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                border: "1px solid grey",
+                backgroundColor: "lightpink",
+              }}
+            ></div>{" "}
+            <h2> Factory worker</h2>
+          </div>
+          <div
+            style={{
+              width: "100%",
+            }}
+          >
+            {" "}
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                border: "1px solid grey",
+                backgroundColor: "lightblue",
+              }}
+            ></div>
+            <h2> Security Personell</h2>
+          </div>
+          <br />
+        </div>
       </div>
-      </div>
-      { showFilter && <SkillToWorker workers={workers} search={search} />}
-        
-     
-    
-      
+      {showFilter && <SkillToWorker workers={workers} search={search} />}
     </>
   );
 }
--- codefury/src/components/SkillToWorker.js
+++ codefury/src/components/SkillToWorker.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 export default function SkillToWorker({ workers, search }) {
- 
   const [filteredSkills, setFilteredSkills] = useState([]);
   useEffect(() => {
     setFilteredSkills(
@@ -13,40 +12,47 @@ export default function SkillToWorker({ workers, search }) {
 
   return (
     <div>
-    
-       <div
-          style={{
-            width: "100%",
-            height: "auto",
-            display: "flex",
-            justifyContent: "center",
-            flexWrap:"wrap",
-            alignItems:"center",
-            paddingTop: "30px",
-          }}
-        >
-      {filteredSkills.map((worker) => (
-       
+      <div
+        style={{
+          width: "100%",
+          height: "auto",
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          alignItems: "center",
+          paddingTop: "30px",
+        }}
+      >
+        {filteredSkills.map((worker) => (
           <div
-          className="crad"
+            className="crad"
             style={{
-              width:(window.innerWidth>500?"30%":"90%"),
+              width: window.innerWidth > 500 ? "30%" : "90%",
               boxShadow: "0px 0px 20px 3px rgb(0,0,0,0.1)",
-              backgroundColor:"white",
+              border: "1px solid black",
+              backgroundColor: "white",
               padding: "50px",
-              margin:10
+              margin: 10,
             }}
           >
-            <h3>{worker.name}</h3>
-            <hr/>
+            <h3 style={{ textTransform: "capitalize" }}>{worker.name}</h3>
+            <hr />
             <h4>Aadhar number : {worker.aadhar_id}</h4>
             <h4>Occupation : {worker.skills}</h4>
             <h4>Village : {worker.village}</h4>
-            <button>Hire</button>
+            <button
+              style={{
+                padding: "7px 20px",
+                borderRadius: "5px",
+                border: "none",
+                backgroundColor: "lightgreen",
+              }}
+            >
+              Hire
+            </button>
           </div>
-           ))}
-        </div>
-     
+        ))}
+      </div>
     </div>
   );
 }
